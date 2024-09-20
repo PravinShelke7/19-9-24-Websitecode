6d9f0644b453123161be4dda12bdc80b674cc9ef
Pravin Shelke <shelkepravin8888@gmail.com>
2024-09-20 11:00:50 +0530

    Ps change EquipmentIN page

--- www.savvypack.com/Pages/Econ1/Assumptions/EquipmentIN.aspx.vb
+++ www.savvypack.com/Pages/Econ1/Assumptions/EquipmentIN.aspx.vb
@@ -14,7 +14,7 @@ Partial Class Pages_Econ1_Assumptions_EquipmentIN
 
     'Ps change 19-9-24 
     ' DT change 19-9-24 4:44PM
-    'Ps change 6.00 PM
+    'Ps change 11.00 AM
     Dim _iCaseId As Integer
     Dim _iUserId As Integer
     Dim _strUserRole As String
